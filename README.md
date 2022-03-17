e8a2cf4457f523d3ff7489479a3d5e04824bb0c3
Valery Crane <zhulery@gmail.com>
2022-03-17 07:12:19 +0500

    The project has no storyboard and contains navigation controller. First viewController has tableView.

---
+++ README.md
@@ -0,0 +1,4 @@
+#  Articles app
+
+- The project has no storyboard and contains navigation controller. First viewController has tableView.
+
---
+++ vazhuravlev_3PW5/ArticleViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ArticleViewController.swift
+//  vazhuravlev_3PW5
+//
+//  Created by Валерий Журавлев on 17.03.2022.
+//
+
+import UIKit
+
+class ArticleViewController: UIViewController {
+    private var tableView: UITableView?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupTableView()
+        // Do any additional setup after loading the view.
+    }
+    
+    private func setupTableView() {
+        let tableView = UITableView()
+        self.view.addSubview(tableView)
+        tableView.pinTop(to: self.view.safeAreaLayoutGuide.topAnchor)
+        tableView.pinBottom(to: self.view.safeAreaLayoutGuide.bottomAnchor)
+        tableView.pin(to: self.view, .left, .right)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        self.tableView = tableView
+    }
+}
+
+extension ArticleViewController: UITableViewDelegate { }
+
+extension ArticleViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    
+}
